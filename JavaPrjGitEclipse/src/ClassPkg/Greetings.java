efaaeb39d2ceddf7cd9ef6985cb5a2653138e057
Lisa Grocki <lisa_grocki@optum.com>
2020-02-02 18:35:21 -0500

    commit 2

--- JavaPrjGitEclipse/src/ClassPkg/Greetings.java
+++ JavaPrjGitEclipse/src/ClassPkg/Greetings.java
@@ -15,6 +15,7 @@ public class Greetings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greeitings LYIT Students - From L00156762");
+		System.out.println("Try 2");
 
 	}
 
