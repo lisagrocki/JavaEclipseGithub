b0ae886a230d847087c7f2309f325a10e6b0b3a8
Lisa Grocki <lisa_grocki@optum.com>
2020-02-02 18:28:53 -0500

    Saving my changes to Git :)

---
+++ JavaPrjGitEclipse/src/ClassPkg/Greetings.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ClassPkg;
+
+/**
+ * @author lgrocki
+ *
+ */
+public class Greetings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greeitings LYIT Students - From L00156762");
+
+	}
+
+}
